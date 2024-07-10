f4acd660146cf4c5ff3410db31dac04e03028d63
aftabbagwan <aftabbagwan9886@gmail.com>
2024-07-10 13:06:58 +0530

    search text field design added

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -9,23 +9,62 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Column(
-          children: [
-            Text(
-              "Search",
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: AppColors.white,
+      // backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  "Search",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: AppColors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 24.0),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 32,
+                      ),
+                      hintText: "Artist,Song or Podcast",
+                      hintStyle: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      prefixIconColor: AppColors.white,
+                      fillColor: Colors.grey.shade800,
+                      focusColor: Colors.grey.shade800,
+                      filled: true,
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      )),
+                ),
+              )
+            ],
+          ),
+          
         ),
       ),
     );
