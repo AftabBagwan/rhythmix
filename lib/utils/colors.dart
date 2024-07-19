3a22b99be335bf06dba57795af9a328ea4e4c514
aftabbagwan <aftabbagwan9886@gmail.com>
2024-07-19 21:35:19 +0530

    added splash screens

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,7 +6,7 @@ class AppColors {
   static Color grey = Colors.grey;
   static Color transparent = Colors.transparent;
   static Color black87 = Colors.black87;
-  static Color lightGreen = Colors.lightGreen;
+  static Color lightGreen = const Color(0xFFC7DFD1);
   static Color greyShade800 = Colors.grey.shade800;
   static Color blueShade100 = Colors.blue.shade100;
   static Color greyShade300 = Colors.grey.shade300;
